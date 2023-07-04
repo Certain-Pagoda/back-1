48e6c7806d830efe0b5a4bdfd822250d26d0c2f7
gonzalogacc <gonzalogacc@gmail.com>
2023-07-04 12:27:37 +0200

    Code adaptation for aws

--- src/models/dynamoDB/users.py
+++ src/models/dynamoDB/users.py
@@ -2,26 +2,42 @@ import os
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, NumberAttribute, UTCDateTimeAttribute, MapAttribute, ListAttribute
 from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+from pathlib import Path
 
 from src.utils.logger import create_logger
 log = create_logger(__name__)
 
 from src.utils.env import load_env
-load_env(os.getenv('ENV_FILE', './.env.local'))
-
+load_env("./.env")
 
 ENV = os.getenv("ENV", None)
 HOST = os.getenv("HOST", None)
 REGION = os.getenv("REGION", None)
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", None)
 
-log.info(f"ENV_FILE: {os.getenv('ENV_FILE', './.env.local')}")
+AWS_ACCESS_KEY_ID_FILE = os.getenv("AWS_ACCESS_KEY_ID_FILE" , None)
+if AWS_ACCESS_KEY_ID_FILE is not None and Path(AWS_ACCESS_KEY_ID_FILE).is_file():
+    log.info("Loading AWS_ACCESS_KEY_ID from file")
+    AWS_ACCESS_KEY_ID = open(AWS_ACCESS_KEY_ID_FILE, 'r').read().strip()
+else:
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", None)
+
+
+AWS_SECRET_ACCESS_KEY_FILE = os.getenv("AWS_SECRET_ACCESS_KEY_FILE" , None)
+if AWS_SECRET_ACCESS_KEY_FILE is not None and Path(AWS_SECRET_ACCESS_KEY_FILE).is_file():
+    log.info("Loading AWS_SECRET_ACCESS_KEY from file")
+    AWS_SECRET_ACCESS_KEY = open(AWS_SECRET_ACCESS_KEY_FILE, 'r').read().strip()
+else:
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY ", None)
+
+
+log.info(f"Getting env variables from ENV_FILE: {os.getenv('ENV_FILE', './.env.local')}")
 log.info(f"ENV: {ENV}")
 log.info(f"HOST: {HOST}")
 log.info(f"REGION: {REGION}")
 log.info(f"AWS_ACCESS_KEY_ID set: {AWS_ACCESS_KEY_ID is not None}")
+log.info(f"AWS_ACCESS_KEY_ID set: {AWS_ACCESS_KEY_ID}")
 log.info(f"AWS_SECRET_ACCESS_KEY set: {AWS_SECRET_ACCESS_KEY is not None}")
+log.info(f"AWS_SECRET_ACCESS_KEY set: {AWS_SECRET_ACCESS_KEY}")
 
 class ConfigurationMetaclass:
         aws_access_key_id = AWS_ACCESS_KEY_ID
--- src/utils/env.py
+++ src/utils/env.py
@@ -1,8 +1,14 @@
 from dotenv import load_dotenv
 from pathlib import Path
+from src.utils.logger import create_logger
+log = create_logger(__name__)
 
 def load_env(env_file: str, fallover=True):
     if Path(env_file).is_file():
+        log.info(f"Loading env variables from ENV_FILE: {env_file}")
         load_dotenv(env_file)
-    elif fallover and Path(".env").is_file():
-        load_dotenv()
+    #elif fallover and Path(".env").is_file():
+    #    load_dotenv()
+    else:
+
+        log.info("No .env file found. Using environment variables.")
